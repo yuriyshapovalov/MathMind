81d2905135dce1f575acf6b02bede5c1d7263a1b
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2013-07-12 23:31:10 +0300

    Added initial catalog structure with some kind of ideas description

---
+++ crowler/bootstrapper.py
@@ -0,0 +1,8 @@
+
+# Bootstrap crowler
+class Bootstrapper:
+    def run:
+        return "running"
+
+    def stop:
+        return "stopped"
---
+++ crowler/crowler.py
@@ -0,0 +1,7 @@
+import httplib
+import urllib
+
+# Responsible for running and controlling crowling process 
+class Crowler:
+    def __init__(self):
+
